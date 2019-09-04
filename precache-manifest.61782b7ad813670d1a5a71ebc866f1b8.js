9e39d8a0112800e34221555231a06e2f6d9f9408
Geoffrey Caven <geoff.caven@nbx.com>
2019-09-04 17:39:59 -0300

    Updates

---
+++ precache-manifest.61782b7ad813670d1a5a71ebc866f1b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d107bd4a92bab10e27cffb46548b959c",
+    "url": "/gorb-roller/index.html"
+  },
+  {
+    "revision": "f000ec9b08f4aaaec3a1",
+    "url": "/gorb-roller/static/css/main.f99d81e0.chunk.css"
+  },
+  {
+    "revision": "37d38958c561478ae086",
+    "url": "/gorb-roller/static/js/2.972787f2.chunk.js"
+  },
+  {
+    "revision": "f000ec9b08f4aaaec3a1",
+    "url": "/gorb-roller/static/js/main.34c7ca0e.chunk.js"
+  },
+  {
+    "revision": "d7c7d266a42e8a611c3d",
+    "url": "/gorb-roller/static/js/runtime~main.6466dea9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08cc5cbd90709776b9bed830d4abf305.js"
+  "/gorb-roller/precache-manifest.61782b7ad813670d1a5a71ebc866f1b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gorb-roller/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
