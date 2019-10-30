8bac574fa091cfe2097b7a44cd6f618b83ab39e2
Geoffrey Caven <geoffreycaven@gmail.com>
2019-10-30 15:28:40 -0300

    Updates

---
+++ precache-manifest.d0b2f3716c3c6b8f924447c78caed907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b414ababd6a80b0dffdf186816a19d60",
+    "url": "/gorb-roller/index.html"
+  },
+  {
+    "revision": "0c7c08bec75539135f7a",
+    "url": "/gorb-roller/static/css/main.f99d81e0.chunk.css"
+  },
+  {
+    "revision": "37d38958c561478ae086",
+    "url": "/gorb-roller/static/js/2.972787f2.chunk.js"
+  },
+  {
+    "revision": "0c7c08bec75539135f7a",
+    "url": "/gorb-roller/static/js/main.a9af6769.chunk.js"
+  },
+  {
+    "revision": "d7c7d266a42e8a611c3d",
+    "url": "/gorb-roller/static/js/runtime~main.6466dea9.js"
+  }
+]);
